a824db19e571e8accfaf9cf4cde76b56023a4d6b
unknown <mat.gold8@gmail.com>
2023-12-11 13:38:59 +0200

    add students view

---
+++ frontend/src/Style/Students.style.tsx
@@ -0,0 +1,24 @@
+import { Button, Table, TableCell, TableContainer, styled } from "@mui/material";
+import { ThemeContextType } from "../Types/types";
+
+export const StudentTable = styled(Table)({
+    width: "70vw",
+});
+
+export const StudentTableContainer = styled(TableContainer)({
+    width: "70vw",
+    boxShadow: "0px 4px 4px 0px #00000040",
+    marginLeft: "15vw",
+    marginTop: "10vh"
+});
+
+export const StyledTableCell = styled(TableCell)({
+    textAlign: "center",
+    fontSize: "1.1rem"
+})
+
+export const DynamicButton = styled(Button)<{projectTheme : ThemeContextType | null}>(({projectTheme}) => ({
+    color: projectTheme === "blue" ? "#3F50B5" : "#F50057",
+    borderColor:  projectTheme === "blue" ? "#3F50B5" : "#F50057",
+    cursor: "pointer",
+}))
--- frontend/src/components/Students.tsx
+++ frontend/src/components/Students.tsx
@@ -1,10 +1,108 @@
-import React from "react"
+import {TableBody, TableHead, TableRow } from "@mui/material";
+import React, {useState} from "react"
+import { useTheme } from "../Context/ThemeContext";
+import * as S from "../Style/Students.style"
+import PopupList from "../components/PopupList"
 
 const Students : React.FC = () => {
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+    const students = [
+        {
+            id: "326570330",
+            firstName: "מתן",
+            lastName: "גולדברג",
+            age: 19,
+            proffestion: "ראפר"
+        },
+        {
+            id: "333222111",
+            firstName: "קווין",
+            lastName: "דה בריינה",
+            age: 33,
+            proffestion: "כדורגלן"
+        },
+        {
+            id: "777888999",
+            firstName: "טיילור",
+            lastName: "סוויפט",
+            age: 32,
+            proffestion: "זמרת"
+        }
+    ];
+
+    const classes = [
+        {
+            id: 1,
+            name: "יוסי"
+        },
+        {
+            id: 2,
+            name: "קרלו"
+        },
+        {
+            id: 3,
+            name: "קיטלרו"
+        }
+    ]
+
+    const openPopup = () : void => {
+        setIsPopupOpen(true);
+    }
+
+    const closePopup = () : void => {
+        setIsPopupOpen(false);
+    }
+
+    const theme = useTheme();
+    console.log(theme)
+
+    const renderedRows = students.map(studnet => {
+        return (
+            <TableRow>
+                <S.StyledTableCell>{studnet.id}</S.StyledTableCell>
+                <S.StyledTableCell>{studnet.firstName}</S.StyledTableCell>
+                <S.StyledTableCell>{studnet.lastName}</S.StyledTableCell>
+                <S.StyledTableCell>{studnet.age}</S.StyledTableCell>
+                <S.StyledTableCell>{studnet.proffestion}</S.StyledTableCell>
+                <S.StyledTableCell>
+                    <S.DynamicButton variant="outlined" projectTheme={theme} onClick={openPopup}>ASSIGN TO CLASS</S.DynamicButton>
+                </S.StyledTableCell>
+                <S.StyledTableCell>
+                    <S.DynamicButton variant="outlined" projectTheme={theme}>DELETE</S.DynamicButton>
+                </S.StyledTableCell>
+            </TableRow>
+        )
+    })
+
     return (
-        <div>
-            students
-        </div>
+       <div>
+        <S.StudentTableContainer>
+            <S.StudentTable>
+                <TableHead>
+                    <TableRow>
+                        <S.StyledTableCell>ID</S.StyledTableCell>
+                        <S.StyledTableCell>First Name</S.StyledTableCell>
+                        <S.StyledTableCell>Last Name</S.StyledTableCell>
+                        <S.StyledTableCell>Age</S.StyledTableCell>
+                        <S.StyledTableCell>Proffestion</S.StyledTableCell>
+                        <S.StyledTableCell>Assign</S.StyledTableCell>
+                        <S.StyledTableCell>Delete</S.StyledTableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {renderedRows}
+                </TableBody>
+            </S.StudentTable>
+        </S.StudentTableContainer>
+        <PopupList
+            isOpen={isPopupOpen} 
+            closeModal={closePopup}
+            items={classes}
+            listType="classes"
+            handleClick={() => console.log("click")}
+            />
+       </div>
     )
 }
 
