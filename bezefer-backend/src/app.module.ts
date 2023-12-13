a9e751cc6fde161925e5801f755fb3f9b81eac53
unknown <mat.gold8@gmail.com>
2023-12-13 11:29:46 +0200

    establish connection to the database

--- bezefer-backend/src/app.module.ts
+++ bezefer-backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(
+    `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/?ssl=${process.env.DB_SSL}&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@${process.env.DB_USERNAME}@`)],
   controllers: [AppController],
   providers: [AppService],
 })
