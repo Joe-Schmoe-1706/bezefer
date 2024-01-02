5ee0559860ef857d1c6e9a92d5e5dced6645c953
unknown <mat.gold8@gmail.com>
2024-01-02 15:10:55 +0200

    add different axios instances for student and class api

--- frontend/src/api/classrooms.api.ts
+++ frontend/src/api/classrooms.api.ts
@@ -1,24 +1,28 @@
 import { Classroom } from "../Types/types";
-import api from "./axiostInstance"
+import axios from "axios";
+
+    const api = axios.create({
+        baseURL: `http://localhost:5000/classes`
+    })
 
     export const getAllClassrooms = async (): Promise<Classroom[]> => {
-        const result = await api.get("/classes")
+        const result = await api.get('')
         const classrooms = await result.data;
         return classrooms;
     }
 
     export const deleteClassroom = async (classroomId: string): Promise<void> => {
-        await api.delete(`/classes/${classroomId}`)
+        await api.delete(`/${classroomId}`)
     }
 
     export const addClassroom = async (classroom: Classroom): Promise<void> => {
-        await api.post('/classes', {
+        await api.post('', {
             classroom
         })
     }
 
     export const getAvailableClassrooms = async (): Promise<Classroom[]> => {
-        const result = await api.get(`/classes/available`);
+        const result = await api.get(`/available`);
         const classrooms = result.data;
         return classrooms;
     }
