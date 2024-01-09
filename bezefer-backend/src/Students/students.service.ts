c7413b0d7d59b522ce2efeac56dafe45184ed092
unknown <mat.gold8@gmail.com>
2024-01-09 12:08:51 +0200

    use transactions

--- bezefer-backend/src/Students/students.service.ts
+++ bezefer-backend/src/Students/students.service.ts
@@ -1,6 +1,6 @@
 import mongoose, { Model } from "mongoose";
 import { BadRequestException, ForbiddenException, Inject, Injectable, NotFoundException } from "@nestjs/common";
-import { InjectModel } from "@nestjs/mongoose";
+import { InjectConnection, InjectModel } from "@nestjs/mongoose";
 import { Student } from "./students.model";
 import { ClassesService } from "../Classes/classes.service";
 import { validate } from "class-validator";
@@ -9,7 +9,7 @@ import { validate } from "class-validator";
 export class StudentsService {
     constructor(
         @InjectModel(Student.name) private readonly studentModel : Model<Student>,
-        @Inject(ClassesService) private readonly classroomService: ClassesService
+        @Inject(ClassesService) private readonly classroomService: ClassesService,
     ) {}
 
     async findAll(): Promise<Student[]> {
@@ -75,6 +75,9 @@ export class StudentsService {
     }
 
     async changeStudentClassStatus(studentId: string, classroomId?: string): Promise<void> {
+        const studentSession = this.studentModel.startSession();
+        (await studentSession).startTransaction();
+
         if (classroomId !== undefined && (await this.classroomService.findClassById(classroomId)).seatsLeft === 0) {
             throw new BadRequestException("there are no available seats in this class");
         } else {
@@ -82,13 +85,16 @@ export class StudentsService {
                 const sutdentToUpdate = await this.getStudentById(studentId);
                 const classroomToChange = classroomId ?? sutdentToUpdate.classroom;
                 sutdentToUpdate.classroom = classroomId ?? '';
-                console.log(sutdentToUpdate);
-                const promises = [];
-                promises.push(sutdentToUpdate.save());
-                promises.push(this.classroomService.changecapacity(classroomToChange, classroomToChange === classroomId ? "add" : "remove"));
-                await Promise.all(promises);
+
+                await sutdentToUpdate.save();
+                await this.classroomService.changecapacity(classroomToChange, classroomToChange === classroomId ? "add" : "remove");
+
+                (await studentSession).commitTransaction();
             } catch (error) {
+                (await studentSession).abortTransaction();
                 throw error;
+            } finally {
+                (await studentSession).endSession();
             }
         }
     }
